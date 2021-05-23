bc3e10b0509dbb10bd60293f574927d9d5680085
Bhargav Lad <ladbhargav97@gmail.com>
2021-05-22 20:36:42 -0400

    Refactor: get method refactored
    refactored get method to functional style

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ struct HashMap<K, V> {
 impl<K, V> HashMap<K, V>
 where
     K: Hash + Eq + Copy, // we just need constrain on key to be hashable
+    V: Copy,
 {
     pub fn new() -> Self {
         HashMap {
@@ -16,7 +17,7 @@ where
             items: 0,
         }
     }
-    fn hash(key: K, buckets_len: usize) -> usize {
+    fn hash(key: &K, buckets_len: usize) -> usize {
         let mut hasher = DefaultHasher::new();
         key.hash(&mut hasher);
         let bucket = (hasher.finish() % buckets_len as u64) as usize;
@@ -31,7 +32,7 @@ where
         new_bucktes.extend((0..target_size).map(|_| Vec::new()));
 
         for (key, val) in self.buckets.iter_mut().flat_map(|bucket| bucket.drain(..)) {
-            let bucket = Self::hash(key, new_bucktes.len());
+            let bucket = Self::hash(&key, new_bucktes.len());
             new_bucktes[bucket].push((key, val));
         }
         let _ = mem::replace(&mut self.buckets, new_bucktes);
@@ -41,7 +42,7 @@ where
         if self.buckets.is_empty() || self.items < 3 * self.buckets.len() / 4 {
             self.resize();
         }
-        let bucket = Self::hash(key, self.buckets.len());
+        let bucket = Self::hash(&key, self.buckets.len());
         let bucket = &mut self.buckets[bucket];
         // for &mut (ref ekey, ref mut evalue) in bucket.iter_mut() {
         for item in bucket.iter_mut() {
@@ -51,7 +52,15 @@ where
             }
         }
         bucket.push((key, value));
+        self.items += 1;
         return None;
+    }
+    pub fn get(&self, key: &K) -> Option<&V> {
+        let bucket_idx = Self::hash(&key, self.buckets.len());
+        self.buckets[bucket_idx]
+            .iter()
+            .find(|(ref ekey, _)| &ekey == &key)
+            .map(|(_, ref val)| val)
     }
 }
 
